3d1a1e620808fa2efb818407b23dc422c285d375
zKnight223 <lazyknight223@gmail.com>
2022-10-11 04:50:17 +1000

    updated deployment json

--- scripts/INKPurchase.deploy.ts
+++ scripts/INKPurchase.deploy.ts
@@ -5,9 +5,9 @@ import inkpurchaseParams from "./deploymentParams/inkpurchase.json"
 async function main() {
 	const INKPurchase = await ethers.getContractFactory("INKPurchase")
 	const inkPurchase = await INKPurchase.deploy(
-		inkpurchaseParams.polygon.treasuryWallet,
-		inkpurchaseParams.polygon.usdc,
-		inkpurchaseParams.polygon.minAmount
+		inkpurchaseParams.ethereum.treasuryWallet,
+		inkpurchaseParams.ethereum.usdc,
+		inkpurchaseParams.ethereum.minAmount
 	)
 
 	await inkPurchase.deployed()
